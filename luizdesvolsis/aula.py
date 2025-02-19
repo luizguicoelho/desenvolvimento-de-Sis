9ca9ae449d76cf33d56deba9e4e856235dd71af9
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-02-19 12:17:30 -0300

    Add files via upload

---
+++ luizdesvolsis/aula.py
@@ -0,0 +1,25 @@
+import random
+
+s = random.randint(1,10)
+
+print("#### BEM VINDO AO JOGO ####")
+print("Adivinhe o número de 1 a 10")
+
+lt = 3
+t = 1
+while (lt >= t):
+    print("tentativa", t)
+    c = int(input("Qual é o número?:" ))
+    if (c == s):
+        print("Parabéns, você acertou!")
+    elif (c > s):
+        print("Quase! tente um menor")
+    elif (c < s):
+        print("Quase! tente um maior")    
+
+
+    t = t + 1
+
+
+print("O seu número é:", s,)    
+   
