a002f5e3e181c2710619777360f5fa11cc69a67b
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-03-14 11:39:20 -0300

    Add files via upload

--- luizdesvolsis/exe1.py
+++ luizdesvolsis/exe1.py
@@ -2,7 +2,11 @@ cuM = [900, 350, 300, 400,]
 gaM = [1400, 700, 500, 200]
 
 def s(Lis):
-    s = Lis[0] + Lis[1] + Lis[2] + Lis[3]
+    s = 0
+    cont = 0
+    while cont < len(Lis):
+        s = s + Lis[cont]
+        cont = cont + 1
     return s 
 
 salP = s(gaM)
