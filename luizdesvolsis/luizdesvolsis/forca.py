614e7d6a3ef26b7c18087d25840a3c6033a31788
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-04-25 10:21:04 -0300

    Add files via upload

--- luizdesvolsis/luizdesvolsis/forca.py
+++ luizdesvolsis/luizdesvolsis/forca.py
@@ -1,73 +1,60 @@
-#importa a biblioteca random
 import random
 
-
 print("Digite o número correspondente do tema escolhido")
-print("##### 1 = Comidas, 2 = nomes, 3 = Dia a Dia#####")
-n_t = int(input("Digite o número escolhiado:"))
+print("##### 1 = Comidas, 2 = esportes, 3 = Roupas #####")
+numero_tema = int(input("Digite o número escolhido: "))
 
-if(n_t == 1):
-    print("Você escolheu o tema comidas")
-    ps = []
-    arq = open("ccomida.txt", "r")
-    for lin in arq:
-        ps.append(lin.strip())
-elif(n_t == 2):
-    print("Você escolheu o tema 'Nomes' ")
-    ps = []
-    arq = open("nome.txt", "r")
-    for lin in arq:
-        ps.append(lin.strip()) 
-elif(n_t == 3):
-    print("Você escolheu o tema 'Dia a Dia' ")
-    ps = []
-    arq = open("diaAdia.txt", "r")
-    for lin in arq:
-        ps.append(lin.strip())
-elif():
-    print("Digito Errado")
-    exit()         
+lista_palavras = []
+if numero_tema == 1:
+    print("Você escolheu o tema 'Comidas'")
+    arquivo = open("comida.txt", "r")
+elif numero_tema == 2:
+    print("Você escolheu o tema 'Nomes'")
+    arquivo = open("esporte.tt", "r")
+elif numero_tema == 3:
+    print("Você escolheu o tema 'Dia a Dia'")
+    arquivo = open("roupa.txt", "r")
+else:
+    print("TU É BURRO POR ACASO?!!!!!")
+    exit()
 
-#configurando inicial das variáveis
-p = random.choice(ps)
-lim_ten = len(p) * 2
-a = False
-e = False
+for linha in arquivo:
+        lista_palavras.append(linha.strip())    
 
-#cria o esqueleto da palavra
-ls_ace = []
-for l in p:
-    ls_ace.append("_")
+palavra_secreta = random.choice(lista_palavras)
 
-#contador
-cont = 1
+limite_tentativas = len(palavra_secreta) * 2
 
-#cri um laço de repetição 
-while(not a and not e):# O jogo continua enquanto o jogador não acertar (a) ou perder (e)
-    print(ls_ace)
-    print("Tentativas: ", cont, "/", lim_ten)
-    c = input("digite a letra: ")
+acertou = False
+errou = False
 
-    #percorre o esqueleto, e se a letra for igual,troca
-    i = 0
-    for l in p:
-        if c == l:
-            ls_ace[i] = c
-        i += 1
-
-    #Condição de derrota
-    if cont == lim_ten :
-        e = True   
-        print("Você perdeu")
-        print("A palavra era: ", p)
-    
-    #Condição de vitória
-    if ls_ace.count("_") == 0:
-        a = True
-        print( "Parabéms, você encontrou a palavra secreta!")
-        print(ls_ace)
-
-    #controle de laço de repetição
-    cont += 1    
+letras_adivinhadas = []
+for letra in palavra_secreta:
+    letras_adivinhadas.append("_")
 
 
+tentativa = 1
+while not acertou and not errou:
+    print(letras_adivinhadas)
+    print("Tentativas:", tentativa, "/", limite_tentativas)
+    letra_digitada = input("Digite uma letra: ")
+
+    indice = 0
+    for letra in palavra_secreta:
+        if letra_digitada == letra:
+            letras_adivinhadas[indice] = letra
+        indice += 1
+
+
+    if tentativa == limite_tentativas:
+        errou = True
+        print("Você perdeu!")
+        print("A palavra era:", palavra_secreta)
+
+
+    if "_" not in letras_adivinhadas:
+        acertou = True
+        print("Parabéns, você encontrou a palavra secreta!")
+        print(letras_adivinhadas)
+
+    tentativa += 1
