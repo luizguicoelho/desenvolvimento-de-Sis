dac6d10e1196e05431bd2cccad1e4eee927db639
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-05-26 12:04:55 -0300

    Add files via upload

---
+++ luizdesvolsis/banco/conta.py
@@ -0,0 +1,44 @@
+class Conta:
+    def __init__(self, agencia, num, titular, saldo, senha):
+        self.__agencia = agencia
+        self.__num = num
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__senha = senha
+
+    def extrato(self):
+        return self.__saldo
+
+    def sacar(self, valor):
+        if valor < 0 or valor > self.__saldo:
+            return False
+        else:
+            self.__saldo -= valor
+            return True     
+
+    def depositar(self, valor):
+        if valor < 0:
+            return False
+        else:    
+            self.__saldo += valor
+            return True 
+
+    def entrar(self, con, sen):
+        if con != self.__num:
+            return False
+        else:
+            if sen != self.__senha:
+                return False
+            else:
+                return True        
+
+    def transferir(self, valor, conta):
+        if valor < 0 or valor > self.__saldo:
+            return False    
+        else:
+            self.__saldo -= valor
+            conta.depositar(valor)
+            return True     
+
+
+        
---
+++ luizdesvolsis/banco/tela.py
@@ -0,0 +1,39 @@
+from conta import Conta
+#import getpass
+#import pwinput
+#import maskpass
+
+conta1 = Conta(123, "10500-10", "Luiz", 7500.0, 1234)
+conta2 = Conta(123, "10500-11", "Guilerme", 1000.0, 1234)
+
+
+con = input('Digite o seu número da conta: ')
+
+sen = int(getpass.getpass('Digite a sua senha: '))
+#sen = int(pwinput.pwinput('Digite a sua senha: '))
+#sen = int(maskpass.askpass(prompt="Digite sua senha: ", mask="*"))
+
+
+conta1 = Conta(123, "10500-10", "Luiz", 7500.0, 1234)
+print(f'Saldo disponivel: {conta1.extrato()}')
+
+if conta1.entrar(con,sen):
+    print("Bem Vindo Luiz")
+else:
+    print("Senha incorreta")   
+
+valor = float(input("Digite o valor que quer depositar:"))
+
+if conta1.transferir(valor, conta2):
+    print(f'Valor de {valor} transferido')
+else:
+    print(f'Tranferencia negada')   
+
+
+print(f'Saldo disponivel: {conta2.extrato()}')
+
+conta1.sacar(2000.0)
+print(f'Saldo disponivel: {conta1.extrato()}')
+
+conta1.depositar(99.0)
+print(f'Saldo disponivel: {conta1.extrato()}')
